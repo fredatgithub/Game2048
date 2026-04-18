9871660101b57201f2879f7810bc4f5394c6d381
fred <domaine75@gmail.com>
2026-04-19 00:24:27 +0200

    add copyright

--- Game2048/Properties/AssemblyInfo.cs
+++ Game2048/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Game2048")]
-[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyCopyright("Copyright ©  2026 MIT Freddy Juhel")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
